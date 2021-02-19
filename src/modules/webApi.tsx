13a624b7763bed8f276370dfd1dcfb6c55ef119d
Anastasia Pedashenko <anastasia1188@mail.ru>
2021-02-19 12:27:24 +0300

    webApi.tsx

--- src/modules/webApi.tsx
+++ src/modules/webApi.tsx
@@ -1,5 +1,3 @@
-import { RefObject } from 'react';
-
 type Func = () => void;
 
 type Props = {
@@ -10,6 +8,7 @@ type Props = {
 };
 
 type HTMLElementFullScreen = HTMLElement & Props;
+type DocumentFullScreen = Document & Props;
 
 const fullScreen = (element: HTMLElementFullScreen) => {
   if (element.requestFullscreen) {
@@ -21,7 +20,34 @@ const fullScreen = (element: HTMLElementFullScreen) => {
   }
 };
 
-export const setWebApi = (elementTarget: RefObject<unknown>) => {
-  const target = elementTarget.current;
-  fullScreen(target as HTMLElementFullScreen);
+const deactivateFullscreen = (document: DocumentFullScreen) => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+};
+
+export const setWebApi = (
+  elementClick: string,
+  elementTarget: string,
+  messageOn = 'На весь экран',
+  messageOff = 'Обычный режим'
+) => {
+  const toggler: HTMLElement | null = document.getElementById(elementClick);
+  const target: HTMLElement | null = document.getElementById(elementTarget);
+
+  if (toggler !== null && target !== null) {
+    toggler.addEventListener('click', () => {
+      if (toggler.innerHTML === messageOn) {
+        toggler.innerHTML = messageOff;
+        fullScreen(target as HTMLElementFullScreen);
+      } else {
+        toggler.innerHTML = messageOn;
+        deactivateFullscreen(document as DocumentFullScreen);
+      }
+    });
+  }
 };
