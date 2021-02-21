5cdebbac16428b8867a70b29a7191f534d657942
Anastasia Pedashenko <anastasia1188@mail.ru>
2021-02-21 21:18:51 +0300

    webApi.tsx

--- src/pages/Game/Game.tsx
+++ src/pages/Game/Game.tsx
@@ -1,8 +1,21 @@
-import React, { memo, useEffect, useRef, useState, useCallback } from 'react';
+import React, {
+  memo,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  MouseEvent
+} from 'react';
 import block from 'bem-cn-lite';
 import { Topping } from '@/components/Topping';
 import { Button } from '@/components/Button';
 import { Engine } from '@/pages/Game/script/Engine';
+import {
+  deactivateFullscreen,
+  fullScreen,
+  HTMLElementFullScreen,
+  DocumentFullScreen
+} from '@/modules/webApi';
 
 import './Game.scss';
 
@@ -10,30 +23,30 @@ const b = block('game');
 
 const Game = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [engine, setEngine] = useState({});
+  const gameRef = useRef<HTMLElementFullScreen>(null);
+
+  const [engine, setEngine] = useState<Engine | null>(null);
   const [isStart, setStart] = useState(false);
   const [isPause, setPause] = useState(false);
-  const [pauseText, setPauseText] = useState('Пауза');
 
   const handlerStart = useCallback(() => {
-    if (isStart) {
-      (engine as Engine).newGame();
-    } else {
-      (engine as Engine).startGame();
+    if (isStart && engine) {
+      engine.newGame();
+      setPause(false);
+    } else if (engine) {
+      engine.startGame();
       setStart(true);
     }
   }, [engine, isStart]);
 
   const handlerPause = useCallback(() => {
     if (engine) {
-      (engine as Engine).pauseGame();
+      engine.pauseGame();
     }
 
     if (isPause) {
-      setPauseText('Пауза');
       setPause(false);
     } else {
-      setPauseText('Продолжить');
       setPause(true);
     }
   }, [engine, isPause]);
@@ -45,42 +58,69 @@ const Game = () => {
 
     setStart(false);
     setPause(false);
-    setPauseText('Пауза');
   }, [engine, isPause]);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-      const engine = new Engine(canvas, ctx);
-      setEngine(engine);
+  const handlerFS = (e: MouseEvent) => {
+    const elemButton = e.target as HTMLElement;
+    const target = gameRef.current;
+    if (elemButton.innerHTML === 'На весь экран') {
+      elemButton.innerHTML = 'Обычный режим';
+      fullScreen(target as HTMLElementFullScreen);
+    } else {
+      elemButton.innerHTML = 'На весь экран';
+      deactivateFullscreen(document as DocumentFullScreen);
     }
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current as HTMLCanvasElement;
+    const engine = new Engine(
+      canvas,
+      canvas.getContext('2d') as CanvasRenderingContext2D
+    );
+    setEngine(engine);
+    // engine.startGame();
+
+    return () => {
+      if (engine && engine.started) {
+        engine.finishGame();
+      }
+    };
   }, []);
 
   return (
     <div className={b()}>
       <Topping title="Игра Pac-Man" />
-      <div className={b('header')}>
-        <Button onClick={handlerStart} size="small game__button">
-          Начать заново
-        </Button>
-        <Button
-          disabled={!isStart}
-          onClick={handlerPause}
-          size="small game__button"
-        >
-          {pauseText}
-        </Button>
-        <Button disabled={!isStart} onClick={handleStop} size="small">
-          Заверишить
-        </Button>
+      <div className={b('game')} ref={gameRef}>
+        <div className={b('header')}>
+          <Button onClick={handlerStart} size="small game__button">
+            Новая игра
+          </Button>
+          <Button
+            disabled={!isStart}
+            onClick={handlerPause}
+            size="small game__button"
+          >
+            {isPause ? 'Продолжить' : 'Пауза'}
+          </Button>
+          <Button
+            disabled={!isStart}
+            onClick={handleStop}
+            size="small game__button"
+          >
+            Завершить
+          </Button>
+          <Button size="small game__button" onClick={handlerFS}>
+            На весь экран
+          </Button>
+        </div>
+        <canvas
+          className={b('canvas')}
+          ref={canvasRef}
+          width={800}
+          height={500}
+        />
       </div>
-      <canvas
-        className={b('canvas')}
-        ref={canvasRef}
-        width={800}
-        height={500}
-      />
     </div>
   );
 };
