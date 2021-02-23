4e41ec7c368f3e83044a421772aaf859ac0b0916
Andrey Zakharov <andzhr@yandex.ru>
2021-02-23 18:11:22 +0300

    Add service worker in project (#57)

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,22 @@ import { App } from '@/components/App';
 
 import '@/styles/main.scss';
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.log(
+          'ServiceWorker registration successful with scope: ',
+          registration.scope
+        );
+      })
+      .catch((error: string) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- webpack.config.js
+++ webpack.config.js
@@ -8,10 +8,13 @@ const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.tsx',
+  entry: {
+    app: './src/index.tsx',
+    sw: './sw.js'
+  },
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.[contenthash].js',
+    filename: '[name].js',
     publicPath: '/'
   },
   resolve: {
@@ -41,10 +44,12 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: './public/index.html',
+      excludeChunks: ['sw'],
+      hash: true
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
+      filename: '[name].css'
     }),
     new WebpackBar()
   ],
