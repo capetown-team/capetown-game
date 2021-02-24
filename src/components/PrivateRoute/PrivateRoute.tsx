53c4d304d16fba606c758a72a9167e47bf3d0562
Inna Badreeva <iibadreeva@mail.ru>
2021-02-24 03:27:46 +0300

    check auth private-route

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -6,7 +6,12 @@ import { ROUTES } from '@/constants';
 import { getAuth } from '@/reducer/user/selectors';
 
 const PrivateRoute = ({ component: Component, ...rest }: RouteProps) => {
-  const isAuth = useSelector(getAuth);
+  const [isAuth, setAuth] = useState(true);
+  const auth = useSelector(getAuth);
+
+  useEffect(() => {
+    setAuth(auth);
+  }, [auth]);
 
   if (!Component) {
     throw new Error('Component is required in PrivateRoute');
