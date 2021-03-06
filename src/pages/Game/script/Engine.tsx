d2ccad4039539db69f1703d787300f909a5c81f4
Inna Badreeva <iibadreeva@mail.ru>
2021-03-06 15:50:23 +0300

    Правки

--- src/pages/Game/script/Engine.tsx
+++ src/pages/Game/script/Engine.tsx
@@ -77,7 +77,7 @@ export class Engine {
     this.blank(ColorType.LightGrey);
     drawText(
       this.ctx,
-      'Вы заверили игру',
+      'Вы завершили игру',
       '17',
       ColorType.Black,
       this.initParameters.width / 2 - 85,
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,31 +1,7 @@
 import { combineReducers } from 'redux';
 import { userReducer } from '@/reducers/user/reducer';
-// import { signinReducer } from '@/reducers/signin/reducer';
-// import { signupReducer } from '@/reducers/signup/reducer';
-// import { profileReducer } from '@/reducers/profile/reducer';
 
 import { UserState } from '@/reducers/user/types';
-// import { ActionProps } from '@/types.d';
-// import { AuthState } from '@/reducers/auth/actions';
-// import { AuthState } from '@/reducers/auth/actions';
-// import { SignInState } from '@/reducers/signin/actions';
-// import { SignUpState } from '@/reducers/signup/actions';
-// import { ProfileState } from '@/reducers/profile/actions';
-
-// export type AppState = {
-//   auth: AuthState;
-//   signin: SignInState;
-//   signup: SignUpState;
-//   profile: ProfileState;
-//   userSettings: ActionProps;
-// };
-//
-// export const reducer = combineReducers({
-//   auth: userReducer,
-//   signin: signinReducer,
-//   signup: signupReducer,
-//   profile: profileReducer
-// });
 
 export type AppState = {
   user: UserState;
