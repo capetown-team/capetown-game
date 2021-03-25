05dfdea136e85279517b4e586a1836054e907de7
Andrey Zakharov <andzhr@yandex.ru>
2021-03-25 23:58:00 +0300

    Fix public path types

--- server/middlewares/hmr-middleware.ts
+++ server/middlewares/hmr-middleware.ts
@@ -5,16 +5,15 @@ import hotMiddleware from 'webpack-hot-middleware';
 import config from '../../webpack/client.config';
 
 const compiler = webpack(config);
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const publicPath: string =
-  config.output && config.output.publicPath ? config.output.publicPath : '/';
 
 export const hmrMiddleware = [
   devMiddleware(compiler, {
     serverSideRender: true,
     writeToDisk: (filePath) => /\.(html|css)$/.test(filePath),
-    publicPath
+    publicPath:
+      config.output && config.output.publicPath
+        ? String(config.output.publicPath)
+        : '/'
   }),
   hotMiddleware(compiler)
 ];
