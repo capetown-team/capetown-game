ca85caa31921f60446a0da4023eb7b6178e5bed6
Anastasia Pedashenko <anastasia1188@mail.ru>
2021-03-26 09:47:02 +0300

    windo.location.origin

--- src/modules/OAuth.tsx
+++ src/modules/OAuth.tsx
@@ -12,7 +12,7 @@ export const getCode = () => {
 export const getCodeOAuth = async () => {
   const responce = await getClientID();
   const clientId = responce.data.service_id;
-  const redirectURL = 'http://localhost:5000/';
+  const redirectURL = window.location.origin;
   const urlAuth = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectURL}`;
   document.location.href = urlAuth;
 };
