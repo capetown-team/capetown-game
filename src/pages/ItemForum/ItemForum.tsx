b27e66ef266ca524949d81f0913787f4881ac6e4
Inna Badreeva <iibadreeva@mail.ru>
2021-04-14 22:01:49 +0300

    static forum with comments

--- src/pages/ItemForum/ItemForum.tsx
+++ src/pages/ItemForum/ItemForum.tsx
@@ -1,17 +1,30 @@
 import React, { memo } from 'react';
-import { Topping } from '@/components/Topping';
+import block from 'bem-cn-lite';
 
+import { Topping } from '@/components/Topping';
 import { PageMeta } from '@/components/PageMeta';
+import { ForumComment } from '@/pages/ItemForum/ForumComment';
+import { Editor } from '@/components/Editor';
 
 import './ItemForum.scss';
 
+const b = block('item-forum');
+
 const ItemForum = () => (
-  <>
+  <div className={b()}>
     <PageMeta title="Форум" description="Какая интересная игра!" />
-    <Topping title="Форум" />
-    <hr />
-    <p>Какая интересная игра!</p>
-  </>
+    <Topping title="Делимся секретами игры" />
+
+    <div className={b('comments')}>
+      <ForumComment right={false} />
+      <ForumComment right={false} />
+      <ForumComment right />
+      <ForumComment right />
+      <ForumComment right={false} />
+    </div>
+
+    <Editor small={false} />
+  </div>
 );
 const WrappedItemForum = memo(ItemForum);
 
