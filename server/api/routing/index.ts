88170d24e9d26aad5d89ea467a9079c0a19ffddc
Andrey Zakharov <andzhr@yandex.ru>
2021-04-18 02:01:01 +0300

    Add theme manage api

--- server/api/routing/index.ts
+++ server/api/routing/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { forumRouter } from './forumRouter';
 import { themeRouter } from './themeRouter';
+import { themeManageRouter } from './themeManageRouter';
 
 const apiRouter: Router = Router();
 
 forumRouter(apiRouter);
 themeRouter(apiRouter);
+themeManageRouter(apiRouter);
 
 export default apiRouter;
